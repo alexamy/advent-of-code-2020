63735b69b1967e292e8a658b623eb6302dedc83f
Alexander Chaschin <ac@happynumbers.com>
2023-08-12 00:59:35 +0300

    add first solution

--- report-repair/src/main.rs
+++ report-repair/src/main.rs
@@ -1,17 +1,30 @@
 use std::fs;
 
 fn main() {
+    println!("Hello, world!");
+}
+
+fn solve() -> u32 {
     let input_path = env!("CARGO_MANIFEST_DIR").to_owned() + "/input.txt";
     let input = fs::read_to_string(input_path)
         .expect("No input file found");
 
-    let numbers: Vec<u16> = input
+    let numbers: Vec<u32> = input
         .split("\n")
         .filter(|r| !r.is_empty())
         .map(|n| n.parse().unwrap())
         .collect();
 
-    dbg!(numbers);
+    for number in &numbers {
+        let target = 2020 - number;
+        let is_target = numbers.contains(&target);
+        if is_target {
+            let result = number * target;
+            return result;
+        }
+    };
+
+    panic!("No solution found");
 }
 
 #[cfg(test)]
@@ -19,7 +32,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn solve() {
-        main();
+    fn first_solution() {
+        assert_eq!(solve(), 902451);
     }
 }
