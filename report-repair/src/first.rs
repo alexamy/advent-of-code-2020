75fc03c60faec551c6fd07e599bd447d2517e142
Alexander Chaschin <ac@happynumbers.com>
2023-08-12 01:07:42 +0300

    extract solution to a file

---
+++ report-repair/src/first.rs
@@ -0,0 +1,25 @@
+use std::fs;
+
+pub fn solve() -> u32 {
+    let directory = env!("CARGO_MANIFEST_DIR");
+    let input_path = format!("{directory}/input.txt");
+    let input = fs::read_to_string(input_path)
+        .expect("No input file found");
+
+    let numbers: Vec<u32> = input
+        .split("\n")
+        .filter(|r| !r.is_empty())
+        .map(|n| n.parse().unwrap())
+        .collect();
+
+    for number in &numbers {
+        let target = 2020 - number;
+        let is_target = numbers.contains(&target);
+        if is_target {
+            let result = number * target;
+            return result;
+        }
+    };
+
+    panic!("No solution found");
+}
--- report-repair/src/main.rs
+++ report-repair/src/main.rs
@@ -1,33 +1,7 @@
+mod first;
+
 fn main() {
     println!("First solution: {}", first::solve());
-}
-
-mod first {
-    use std::fs;
-
-    pub fn solve() -> u32 {
-        let directory = env!("CARGO_MANIFEST_DIR");
-        let input_path = format!("{directory}/input.txt");
-        let input = fs::read_to_string(input_path)
-            .expect("No input file found");
-
-        let numbers: Vec<u32> = input
-            .split("\n")
-            .filter(|r| !r.is_empty())
-            .map(|n| n.parse().unwrap())
-            .collect();
-
-        for number in &numbers {
-            let target = 2020 - number;
-            let is_target = numbers.contains(&target);
-            if is_target {
-                let result = number * target;
-                return result;
-            }
-        };
-
-        panic!("No solution found");
-    }
 }
 
 #[cfg(test)]
