fbffecbf56bb49a17e41e120d01737647e991f1d
Alexander Chaschin <ac@happynumbers.com>
2023-08-12 00:48:33 +0300

    add numbers

--- report-repair/src/main.rs
+++ report-repair/src/main.rs
@@ -2,9 +2,16 @@ use std::fs;
 
 fn main() {
     let input_path = env!("CARGO_MANIFEST_DIR").to_owned() + "/input.txt";
-    let numbers = fs::read_to_string(input_path)
+    let input = fs::read_to_string(input_path)
         .expect("No input file found");
 
+    let numbers: Vec<i16> = input
+        .split("\n")
+        .filter(|r| !r.is_empty())
+        .map(|n| n.parse().unwrap())
+        .collect();
+
+    dbg!(numbers);
 }
 
 #[cfg(test)]
