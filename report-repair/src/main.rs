e712123e64535f2725199e7f5cf0883ee5537652
Alexander Chaschin <ac@happynumbers.com>
2023-08-12 00:42:27 +0300

    move input in package

--- report-repair/src/main.rs
+++ report-repair/src/main.rs
@@ -1,3 +1,18 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input_path = env!("CARGO_MANIFEST_DIR").to_owned() + "/input.txt";
+    let numbers = fs::read_to_string(input_path)
+        .expect("No input file found");
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn solve() {
+        main();
+    }
 }
