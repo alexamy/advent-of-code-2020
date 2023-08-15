5cb389315917f67eb816ecd975110c5581e6d3f4
Alexander Chaschin <ac@happynumbers.com>
2023-08-15 11:22:49 +0300

    use references

--- rust_playground/src/main.rs
+++ rust_playground/src/main.rs
@@ -2,8 +2,10 @@ use std::collections::HashMap;
 
 fn main() {
     let input = get_input();
-    let vec = split_whitespace(input);
+    let vec = split_whitespace(&input);
     let parts = make_hash(vec);
+
+    println!("{}", input);
 
     dbg!(parts);
 }
@@ -12,14 +14,14 @@ fn get_input() -> String {
     String::from("Hello:from here:there")
 }
 
-fn split_whitespace(input: String) -> Vec<String> {
-    input.split(" ").map(str::to_string).collect()
+fn split_whitespace(input: &str) -> Vec<&str> {
+    input.split(" ").collect()
 }
 
-fn make_hash(vec: Vec<String>) -> HashMap<String, String> {
+fn make_hash(vec: Vec<&str>) -> HashMap<&str, &str> {
     vec.iter()
         .map(|s| {
-            let mut entries = s.split(':').map(str::to_string);
+            let mut entries = s.split(':');
             let key = entries.next().unwrap();
             let val = entries.next().unwrap();
             (key, val)
