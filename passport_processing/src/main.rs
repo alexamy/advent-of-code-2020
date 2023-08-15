43777bc30e7eb80d6b0764529cc1ec2168bc12ba
Alexander Chaschin <ac@happynumbers.com>
2023-08-15 23:39:06 +0300

    add second solution

--- passport_processing/src/main.rs
+++ passport_processing/src/main.rs
@@ -1,10 +1,10 @@
 mod first;
 mod processor;
 mod reader;
+mod second;
 mod solution;
-// mod second;
 
 fn main() {
     println!("First solution: {}", first::solve());
-    // println!("Second solution: {}", second::solve());
+    println!("Second solution: {}", second::solve());
 }
---
+++ passport_processing/src/second.rs
@@ -0,0 +1,27 @@
+use crate::{processor, reader, solution::Passport};
+
+pub fn solve() -> u32 {
+    let input = reader::read_input();
+    let passports = processor::split_passports(&input);
+
+    let entries: Vec<_> = passports
+        .iter()
+        .map(|p| processor::split_entries(p))
+        .map(Passport::new)
+        .filter(Option::is_some)
+        .map(Option::unwrap)
+        .filter(Passport::is_valid)
+        .collect();
+
+    entries.len() as u32
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_solved() {
+        assert_eq!(solve(), 133);
+    }
+}
