f3141f304315958b5a5507f95b80831f93490c5f
Alexander Chaschin <ac@happynumbers.com>
2023-08-15 10:47:50 +0300

    add rust playground

---
+++ rust_playground/src/main.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+
+fn main() {
+    let input = get_input();
+    let vec = split_whitespace(input);
+    let parts = make_hash(vec);
+
+    dbg!(parts);
+}
+
+fn get_input() -> String {
+    String::from("Hello:from here:there")
+}
+
+fn split_whitespace(input: String) -> Vec<String> {
+    input.split(" ").map(|s| s.to_string()).collect()
+}
+
+fn make_hash(vec: Vec<String>) -> HashMap<String, String> {
+    vec.iter()
+        .map(|s| {
+            let mut entries = s.split(':').map(|s| s.to_string());
+            let key = entries.next().unwrap();
+            let val = entries.next().unwrap();
+            (key, val)
+        })
+        .collect()
+}
