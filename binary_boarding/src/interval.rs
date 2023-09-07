2b5971296310da29dbed1c930b2c4bda42ab26de
Alexander Chaschin <ac@happynumbers.com>
2023-09-07 22:43:33 +0200

    add converge

--- binary_boarding/src/interval.rs
+++ binary_boarding/src/interval.rs
@@ -18,6 +18,14 @@ impl Interval {
             max: self.max,
         }
     }
+
+    pub fn converged(&self) -> Result<u32, &str> {
+        if self.min == self.max {
+            Ok(self.min)
+        } else {
+            Err("Interval is not converged")
+        }
+    }
 }
 
 #[cfg(test)]
@@ -34,5 +42,11 @@ mod tests {
     fn is_calculate_right() {
         let interval = Interval { min: 0, max: 127 };
         assert_eq!(interval.right(), Interval { min: 64, max: 127 });
+    }
+
+    #[test]
+    fn get_converged_value() {
+        let interval = Interval { min: 1, max: 1 };
+        assert_eq!(interval.converged(), Ok(1));
     }
 }
