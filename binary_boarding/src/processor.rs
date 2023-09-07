ddb360e8ce22da47d4b978cd9220a88c47939444
Alexander Chaschin <ac@happynumbers.com>
2023-09-07 22:18:12 +0200

    add calc id

--- binary_boarding/src/processor.rs
+++ binary_boarding/src/processor.rs
@@ -6,7 +6,17 @@ struct Seat {
 }
 
 impl Seat {
-    fn parse(code: &str) -> Self {}
+    pub fn parse(code: &str) -> Self {
+        let row = 0;
+        let column = 0;
+        let id = Self::calculate_id(row, column);
+
+        Seat { row, column, id }
+    }
+
+    fn calculate_id(row: u32, column: u32) -> u32 {
+        row * 8 + column
+    }
 }
 
 #[cfg(test)]
