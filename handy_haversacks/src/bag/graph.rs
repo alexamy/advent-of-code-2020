0465e1ffdb84104f198a5f09529e8c4f548f48d2
Alexander Chaschin <ac@happynumbers.com>
2023-09-13 23:10:56 +0200

    add bag graph test

--- handy_haversacks/src/bag/graph.rs
+++ handy_haversacks/src/bag/graph.rs
@@ -1,4 +1,36 @@
-struct Bag {
-    color: String,
-    bags: Vec<Box<Bag>>,
+#[derive(Debug, PartialEq)]
+pub struct Bag<'a> {
+    pub count: u32,
+    pub color: &'a str,
+    pub bags: Vec<Box<Bag<'a>>>,
+}
+
+pub fn parse(infos: Vec<&str>) -> Bag {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_parsed() {
+        assert_eq!(
+            parse(Vec::from([
+                "light red bags contain 2 bright white bag.",
+                "bright white bags contain 1 shiny gold bag."
+            ])),
+            Bag {
+                count: 1,
+                color: "light red",
+                bags: Vec::from([Box::new(Bag {
+                    count: 2,
+                    color: "bright white",
+                    bags: Vec::from([Box::new(Bag {
+                        count: 1,
+                        color: "shiny gold",
+                        bags: Vec::new(),
+                    })]),
+                })]),
+            }
+        )
+    }
 }
