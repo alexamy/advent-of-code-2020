4bbf507d5e6a63f031b48127c3cefa2665616970
Alexander Chaschin <ac@happynumbers.com>
2023-09-14 21:39:14 +0200

    add interpret

--- handheld_halting/src/interpreter.rs
+++ handheld_halting/src/interpreter.rs
@@ -1,9 +1,37 @@
-use crate::parser;
+use crate::parser::{self, Instruction};
+use std::collections::HashMap;
 
 pub fn solve(input: &str) -> i32 {
     let instructions = parser::parse(input);
 
-    0
+    interpret(instructions)
+}
+
+fn interpret(instructions: Vec<Instruction>) -> i32 {
+    let mut accumulator: i32 = 0;
+    let mut position: i32 = 0;
+    let mut counts = HashMap::new();
+
+    loop {
+        if counts.contains_key(&position) {
+            return accumulator;
+        }
+
+        counts
+            .entry(position)
+            .and_modify(|count| *count += 1)
+            .or_insert(1);
+
+        let instruction = &instructions[position as usize];
+        let mut next = 1;
+        match instruction {
+            Instruction::Nop(_) => (),
+            Instruction::Acc(offset) => accumulator += offset,
+            Instruction::Jmp(offset) => next = *offset,
+        }
+
+        position += next;
+    }
 }
 
 #[cfg(test)]
@@ -11,7 +39,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn is_solved() {
+    fn is_solved_simple() {
         let input = "\
 nop +0
 acc +1
