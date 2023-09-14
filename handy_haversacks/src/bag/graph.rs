8735761a3e5133edc7921218d143a5d8a0db13da
Alexander Chaschin <ac@happynumbers.com>
2023-09-14 15:09:57 +0200

    use some

--- handy_haversacks/src/bag/graph.rs
+++ handy_haversacks/src/bag/graph.rs
@@ -1,28 +1,27 @@
+use crate::bag::parser;
 use std::collections::HashMap;
-
-use super::parser;
 
 #[derive(Debug, PartialEq)]
 pub struct Bag<'a> {
     pub color: &'a str,
-    pub bags: Vec<Box<&'a Bag<'a>>>,
+    pub bags: Vec<Box<Bag<'a>>>,
 }
 
 pub fn parse(infos: Vec<&str>) -> Vec<Bag> {
     let mut hash = HashMap::new();
     let rows = infos.iter().map(|line| parser::parse(line));
 
-    let mut bags = Vec::new();
+    let mut result = Vec::new();
     for parser::Row { color, bags } in rows {
         let parent = hash.entry(color).or_insert_with(|| {
             let mut result = Vec::new();
-            for parser::Info { color, count } in bags {
+            for parser::Info { color, count: _ } in bags {
                 let child = hash.entry(color).or_insert(Bag {
                     color,
                     bags: Vec::new(),
                 });
 
-                result.push(Box::new(child));
+                result.push(Box::new(*child));
             }
 
             Bag {
@@ -32,7 +31,7 @@ pub fn parse(infos: Vec<&str>) -> Vec<Bag> {
         });
     }
 
-    bags
+    result
 }
 
 #[cfg(test)]
@@ -48,12 +47,12 @@ mod tests {
 
         let bright_white = Bag {
             color: "bright white",
-            bags: Vec::from([Box::new(&shiny_gold)]),
+            bags: Vec::from([Box::new(shiny_gold)]),
         };
 
         let light_red = Bag {
             color: "light red",
-            bags: Vec::from([Box::new(&bright_white)]),
+            bags: Vec::from([Box::new(bright_white)]),
         };
 
         assert_eq!(
