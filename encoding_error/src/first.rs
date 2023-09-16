5026051fecb86795539579a3a93bcd2c204a6208
Alexander Chaschin <ac@happynumbers.com>
2023-09-16 21:12:45 +0200

    add some solution

--- encoding_error/src/first.rs
+++ encoding_error/src/first.rs
@@ -1,5 +1,36 @@
+use core::num;
+
 fn find_breaking(numbers: Vec<u32>, preamble_length: u32) -> Option<u32> {
+    for (i, number) in numbers.iter().enumerate() {
+        let end_index = i + preamble_length as usize;
+        if end_index > numbers.len() {
+            return None;
+        }
+
+        let candidates = &numbers[i..end_index];
+        let is_proper = is_contained(candidates, *number);
+        if !is_proper {
+            return Some(*number);
+        }
+    }
+
     None
+}
+
+fn is_contained(numbers: &[u32], target: u32) -> bool {
+    for (ci, c1) in numbers.iter().enumerate() {
+        if ci == numbers.len() - 1 {
+            return false;
+        }
+
+        for c2 in &numbers[ci + 1..] {
+            if c1 + c2 == target {
+                return true;
+            }
+        }
+    }
+
+    return false;
 }
 
 #[cfg(test)]
