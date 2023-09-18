35ae99d212fa13ca10a20ad97ad9a3cdb7238b77
Alexander Chaschin <ac@happynumbers.com>
2023-09-19 00:32:40 +0200

    add map convert

--- seating_system/src/solver.rs
+++ seating_system/src/solver.rs
@@ -1,10 +1,42 @@
+#[derive(Debug, PartialEq)]
+enum Cell {
+    Floor,
+    Empty,
+    Occupied,
+}
+
 pub fn count_seats(map: &str) -> u32 {
+    let map = convert_map(map);
+
     0
+}
+
+fn convert_map(map: &str) -> Vec<Vec<Cell>> {
+    map.split("\n")
+        .map(|line| line.chars().map(get_cell).collect())
+        .collect()
+}
+
+fn get_cell(character: char) -> Cell {
+    match character {
+        '.' => Cell::Floor,
+        'L' => Cell::Empty,
+        '#' => Cell::Occupied,
+        c => panic!("Unknown cell character: {}", c),
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn converts_map() {
+        assert_eq!(
+            convert_map(".L#"),
+            vec![vec![Cell::Floor, Cell::Empty, Cell::Occupied]],
+        );
+    }
 
     #[test]
     fn seats_are_counted() {
