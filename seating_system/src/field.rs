3438e48de6a6e1cb02d5b1c7025bbeb49f4ce4c2
Alexander Chaschin <ac@happynumbers.com>
2023-09-22 19:57:57 +0200

    rewrite field and cell

--- seating_system/src/field.rs
+++ seating_system/src/field.rs
@@ -5,21 +5,27 @@ pub enum Cell {
     Occupied,
 }
 
-pub type Field = Vec<Vec<Cell>>;
-
-pub fn convert_field(field: &str) -> Field {
-    field
-        .split("\n")
-        .map(|line| line.chars().map(get_cell_type).collect())
-        .collect()
+impl Cell {
+    pub fn from_character(character: char) -> Self {
+        match character {
+            '.' => Cell::Floor,
+            'L' => Cell::Empty,
+            '#' => Cell::Occupied,
+            c => panic!("Unknown cell character: {}", c),
+        }
+    }
 }
 
-fn get_cell_type(character: char) -> Cell {
-    match character {
-        '.' => Cell::Floor,
-        'L' => Cell::Empty,
-        '#' => Cell::Occupied,
-        c => panic!("Unknown cell character: {}", c),
+#[derive(Debug, PartialEq)]
+pub struct Field(Vec<Vec<Cell>>);
+
+impl Field {
+    pub fn from_map(map: &str) -> Self {
+        Self(
+            map.split("\n")
+                .map(|line| line.chars().map(Cell::from_character).collect())
+                .collect(),
+        )
     }
 }
 
@@ -30,8 +36,8 @@ mod tests {
     #[test]
     fn converts_map() {
         assert_eq!(
-            convert_field(".L#"),
-            vec![vec![Cell::Floor, Cell::Empty, Cell::Occupied]],
+            Field::from_map(".L#"),
+            Field(vec![vec![Cell::Floor, Cell::Empty, Cell::Occupied]]),
         );
     }
 }
