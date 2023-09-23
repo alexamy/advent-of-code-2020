2af68b8000cbed2a20c560812010f678e5f02d8e
Alexander Chaschin <ac@happynumbers.com>
2023-09-23 12:13:21 +0200

    add go

--- rain_risk/src/turtle.rs
+++ rain_risk/src/turtle.rs
@@ -15,11 +15,30 @@ struct Turtle {
     position: Position,
 }
 
+enum Directive {
+    North(i32),
+    South(i32),
+    East(i32),
+    West(i32),
+    Right(i32),
+    Left(i32),
+    Forward(i32),
+}
+
 impl Turtle {
     pub fn new() -> Self {
         Self {
             direction: Direction::East,
             position: Position { x: 0, y: 0 },
+        }
+    }
+
+    pub fn go(&self, directive: Directive) {
+        match directive {
+            Directive::South(n) => self.position.y += n,
+            Directive::North(n) => self.position.y -= n,
+            Directive::East(n) => self.position.x += n,
+            Directive::West(n) => self.position.x -= n,
         }
     }
 }
